600ddd14c3b2b33c0cfeea201771ae365f017b76
rimb05 <rim@drumagog.com>
2024-05-17 08:48:05 -0500

    used DB link

--- rootfs.sh
+++ rootfs.sh
@@ -29,7 +29,7 @@ cd "${DIR}/dl"
 if [ -f "${DIR}/dl/${rootfs_name}" ] ; then
     echo "File downloaded: ${DIR}/dl/${rootfs_name}"
 else
-    wget "https://drive.usercontent.google.com/download?id=1kEco22WrjYhaFoAfxaGbd41blzu6kYSC&export=download&confirm=t"  -O ${rootfs_name}
+    wget "https://www.dropbox.com/scl/fi/gda270cg1iarugi5tcc5k/ubuntu-22.04-base-stm32mp1-armhf-16-05-2022.tar.gz?rlkey=6fd7yr7obl4ijqr3uqii1hgek&dl=0"  -O ${rootfs_name}dl
 fi
 cd "${DIR}"
 exit 0 ;
